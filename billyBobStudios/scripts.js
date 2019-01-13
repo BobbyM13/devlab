583815b09ff6fd01c890048c86375eb219fc62c7
BobbyM13 <BobbyM13@users.noreply.github.com>
2019-01-13 01:23:19 -0600

    Updated Mobile Nav and Made 4 Pages Fully Responsive

---
+++ billyBobStudios/scripts.js
@@ -0,0 +1,48 @@
+const navColor = document.getElementById("colorNav");
+const subNav = document.getElementById("hiddenEle");
+const mobileSubNav = document.getElementById("projOpen");
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+
+    console.log(scrolled);
+
+    if (scrollY > 200) {
+        navColor.style.backgroundColor = "white";
+        navColor.classList.add("black");
+        navColor.style.transition = ".2s";
+    } else {
+        navColor.style.backgroundColor = "";
+        navColor.classList.remove("black");
+        navColor.style.transition = ".2s";
+    }
+});
+
+document.getElementById('projects').addEventListener('mouseenter', openMenu);
+
+function openMenu() {
+    document.getElementById('subNav').classList.toggle('active');
+};
+
+document.getElementById('projects').addEventListener('mouseleave', closeMenu);
+
+function closeMenu() {
+    document.getElementById('subNav').classList.toggle('active');
+};
+
+function openNav() {
+    document.getElementById('fun').style.width = "100%";
+    document.getElementById('openBtn').style.opacity = "0";
+};
+
+function closeNav() {
+    document.getElementById('fun').style.width = "0";
+    document.getElementById('openBtn').style.opacity = "1";
+};
+
+mobileSubNav.addEventListener('click', ()=> {
+
+const subItems = document.getElementById("subSection")
+subItems.classList.toggle("active");
+
+});
